6cc0265c1dc53f88d55b70ee14ff30a89fc755dd
Aaryan-f20250693 <f20250693@pilani.bits-pilani.ac.in>
2026-08-28 18:30:34 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -1,6 +1,4 @@
 // cla4.v
-// (Carried forward from Task 3 -- paste in your completed, delay-annotated
-// version.)
 // Gate-level 4-bit carry-lookahead adder, matching the lecture circuit.
 // Every gate needs an explicit delay (constant is fine here, e.g. #(2)) --
 // this is the default from Task 2 onward, not a special step.
@@ -35,5 +33,41 @@ module cla4(
 
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
+
+  xor #(2) (p0, a[0], b[0]);
+  xor #(2) (p1, a[1], b[1]);
+  xor #(2) (p2, a[2], b[2]);
+  xor #(2) (p3, a[3], b[3]);
+
+  and #(2) (g0, a[0], b[0]);
+  and #(2) (g1, a[1], b[1]);
+  and #(2) (g2, a[2], b[2]);
+  and #(2) (g3, a[3], b[3]);
+
+  wire w0;
+  and #(2) (w0, p0, cin);
+  or #(2) (c1, w0, g0);
+  xor #(2) (sum[0], cin, p0);
+
+  wire w1, w2;
+  and #(2) (w1, p1, g0);
+  and #(2) (w2, p1, p0, cin);
+  or #(2) (c2, w1, w2, g1);
+  xor #(2) (sum[1], c1, p1);
+
+  wire w3, w4, w5;
+  and #(2) (w3, p2, g1);
+  and #(2) (w4, p2, p1, g0);
+  and #(2) (w5, p2, p1, p0, cin);
+  or #(2) (c3, w3, w4, w5, g2);
+  xor #(2) (sum[2], c2, p2);
+
+  wire w6, w7, w8, w9;
+  and #(2) (w6, p3, g2);
+  and #(2) (w7, p3, p2, g1);
+  and #(2) (w8, p3, p2, p1, g0);
+  and #(2) (w9, p3, p2, p1, p0, cin);
+  or #(2) (cout, w6, w7, w8, w9, g3);
+  xor #(2) (sum[3], c3, p3);
 
 endmodule
